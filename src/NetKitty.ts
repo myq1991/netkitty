6ebed5192a388aab867468e7e62adfe30bbeeefe
myq1991 <myq1991@gmail.com>
2025-03-29 23:57:46 +0800

    Add multiple exports for codec utilities in NetKitty module.

--- src/NetKitty.ts
+++ src/NetKitty.ts
@@ -1,3 +1,63 @@
+export {BufferToHex} from './lib/codec/lib/BufferToHex'
+export {BufferToIPv4, BufferToIPv6} from './lib/codec/lib/BufferToIP'
+export {
+    BufferToFloat32,
+    BufferToInt16,
+    BufferToInt32,
+    BufferToInt64,
+    BufferToInt8,
+    BufferToUInt16, BufferToUInt32, BufferToUInt64,
+    BufferToUInt8
+} from './lib/codec/lib/BufferToNumber'
+export {FixHexString} from './lib/codec/lib/FixHexString'
+export {FlexibleObject} from './lib/codec/lib/FlexibleObject'
+export {
+    HexToFloat32,
+    HexToInt16,
+    HexToInt32,
+    HexToInt64,
+    HexToInt8,
+    HexToUInt16,
+    HexToUInt32, HexToUInt64,
+    HexToUInt8
+} from './lib/codec/lib/HexToNumber'
+export {IPv4ToBuffer, IPv6ToBuffer} from './lib/codec/lib/IPToBuffer'
+export {
+    Int16ToBERBuffer,
+    Int32ToBERBuffer,
+    Int64ToBERBuffer,
+    Int8ToBERBuffer, UInt16ToBERBuffer, UInt32ToBERBuffer, UInt64ToBERBuffer,
+    UInt8ToBERBuffer
+} from './lib/codec/lib/NumberToBERBuffer'
+export {
+    Int16ToBERHex,
+    Int32ToBERHex,
+    Int64ToBERHex,
+    Int8ToBERHex,
+    UInt16ToBERHex, UInt32ToBERHex, UInt64ToBERHex,
+    UInt8ToBERHex
+} from './lib/codec/lib/NumberToBERHex'
+export {
+    Float32ToBuffer,
+    Int16ToBuffer,
+    Int32ToBuffer,
+    Int64ToBuffer,
+    Int8ToBuffer,
+    UInt16ToBuffer, UInt32ToBuffer, UInt64ToBuffer,
+    UInt8ToBuffer
+} from './lib/codec/lib/NumberToBuffer'
+export {
+    Float32ToHex,
+    Int16ToHex,
+    Int32ToHex,
+    Int64ToHex,
+    Int8ToHex,
+    UInt16ToHex,
+    UInt32ToHex, UInt64ToHex,
+    UInt8ToHex
+} from './lib/codec/lib/NumberToHex'
+export {StringContentEncodingEnum} from './lib/codec/lib/StringContentEncodingEnum'
+
 export {type INetworkInterface} from './lib/nodepcap/interfaces/INetworkInterface'
 export {type CodecEncodeResult} from './lib/codec/types/CodecEncodeResult'
 export {type CodecEncodeInput} from './lib/codec/types/CodecEncodeInput'
@@ -29,3 +89,4 @@ export {Capture} from './lib/nodepcap/Capture'
  * Network packet codec
  */
 export {Codec, Headers} from './lib/codec/Codec'
+
